dd0b2490683c546c12f2ea833c293c2a0e474ea8
Joe Blau <josephblau@gmail.com>
2016-08-22 08:01:20 -0400

    Updated external macros

--- external-project-macros.cmake
+++ external-project-macros.cmake
@@ -25,13 +25,11 @@ endmacro()
 # Eigen fetch and install
 #
 macro(install_eigen)
-  set(eigen_url http://www.vtk.org/files/support/eigen-3.1.0-alpha1.tar.gz)
-  set(eigen_md5 c04dedf4ae97b055b6dd2aaa01daf5e9)
+  set(eigen_url http://bitbucket.org/eigen/eigen/get/3.2.8.tar.bz2)
   ExternalProject_Add(
     eigen
     SOURCE_DIR ${source_prefix}/eigen
     URL ${eigen_url}
-    URL_MD5 ${eigen_md5}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory "${source_prefix}/eigen/Eigen" "${install_prefix}/eigen/Eigen" && ${CMAKE_COMMAND} -E copy_directory "${source_prefix}/eigen/unsupported" "${install_prefix}/eigen/unsupported"
@@ -46,7 +44,7 @@ macro(fetch_vtk)
     vtk-fetch
     SOURCE_DIR ${source_prefix}/vtk
     GIT_REPOSITORY git://github.com/Kitware/VTK.git
-#    GIT_TAG ce4a267
+    GIT_TAG v6.3.0
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
@@ -69,6 +67,27 @@ macro(compile_vtk)
       -DCMAKE_BUILD_TYPE:STRING=${build_type}
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_TESTING:BOOL=OFF
+      -DVTK_ANDROID_BUILD:BOOL=ON
+      -DANDROID_ARCH_NAME:STRING=armeabi-v7a
+      -DANDROID_NATIVE_API_LEVEL:STRING=21
+      -DOPENGL_ES_VERSION:STRING=2.0
+      -DVTK_EXTRA_COMPILER_WARNINGS:BOOL=OFF
+      -DVTK_Group_Imaging:BOOL=OFF
+      -DVTK_Group_MPI:BOOL=OFF
+      -DVTK_Group_Qt:BOOL=OFF
+      -DVTK_Group_Rendering:BOOL=OFF
+      -DVTK_Group_StandAlone:BOOL=ON
+      -DVTK_Group_Tk:BOOL=OFF
+      -DVTK_Group_Views:BOOL=OFF
+      -DVTK_Group_Web:BOOL=OFF
+      -DVTK_IOS_BUILD:BOOL=OFF
+      -DVTK_PYTHON_VERSION:STRING=2
+      -DVTK_RENDERING_BACKEND:STRING=OpenGL
+      -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=Sequential
+      -DVTK_USE_LARGE_DATA:BOOL=OFF
+      -DVTK_WRAP_JAVA:BOOL=OFF
+      -DVTK_WRAP_PYTHON:BOOL=OFF
+      -DVTK_WRAP_TCL:BOOL=OFF
       ${vtk_module_defaults}
   )
 endmacro()
@@ -80,25 +99,56 @@ macro(crosscompile_vtk tag)
   set(proj vtk-${tag})
   get_toolchain_file(${tag})
   get_try_run_results_file(${proj})
+  if(${tag} STREQUAL "android")
+    set(is_Android ON)
+    set(is_iOSDevice OFF)
+    set(is_iOSSimulator ON)
+  elseif(${tag} STREQUAL  "ios_device")
+    set(is_Android OFF)
+    set(is_iOSDevice ON)
+    set(is_iOSSimulator ON)
+  elseif(${tag} STREQUAL  "ios_simulator")
+    set(is_Android OFF)
+    set(is_iOSDevice ON)
+    set(is_iOSSimulator ON)
+  endif ()
+
   ExternalProject_Add(
     ${proj}
     SOURCE_DIR ${source_prefix}/vtk
     DOWNLOAD_COMMAND ""
-    INSTALL_COMMAND ""
     DEPENDS vtk-host
     CMAKE_ARGS
-      -DVTK_IOS_BUILD:BOOL=ON
       -DCMAKE_INSTALL_PREFIX:PATH=${install_prefix}/${proj}
       -DCMAKE_BUILD_TYPE:STRING=${build_type}
       -DBUILD_SHARED_LIBS:BOOL=OFF
       -DBUILD_TESTING:BOOL=OFF
       -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${toolchain_file}
       -DVTKCompileTools_DIR:PATH=${build_prefix}/vtk-host
+      -DVTK_ANDROID_BUILD:BOOL=${is_Android}
+      -DVTK_IOS_BUILD:BOOL=${is_iOSDevice}
+      -DANDROID_ARCH_NAME:STRING=armeabi-v7a
+      -DANDROID_NATIVE_API_LEVEL:STRING=21
+      -DOPENGL_ES_VERSION:STRING=2.0
+      -DVTK_EXTRA_COMPILER_WARNINGS:BOOL=OFF
+      -DVTK_Group_Imaging:BOOL=OFF
+      -DVTK_Group_MPI:BOOL=OFF
+      -DVTK_Group_Qt:BOOL=OFF
+      -DVTK_Group_Rendering:BOOL=OFF
+      -DVTK_Group_StandAlone:BOOL=ON
+      -DVTK_Group_Tk:BOOL=OFF
+      -DVTK_Group_Views:BOOL=OFF
+      -DVTK_Group_Web:BOOL=OFF
+      -DVTK_PYTHON_VERSION:STRING=2
+      -DVTK_RENDERING_BACKEND:STRING=OpenGL
+      -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=Sequential
+      -DVTK_USE_LARGE_DATA:BOOL=OFF
+      -DVTK_WRAP_JAVA:BOOL=OFF
+      -DVTK_WRAP_PYTHON:BOOL=OFF
+      -DVTK_WRAP_TCL:BOOL=OFF
       ${vtk_module_defaults}
       -C ${try_run_results_file}
   )
-
-  force_build(${proj})
 endmacro()
 
 #
@@ -108,8 +158,8 @@ macro(fetch_flann)
   ExternalProject_Add(
     flann-fetch
     SOURCE_DIR ${source_prefix}/flann
-    GIT_REPOSITORY git://github.com/mariusmuja/flann.git
-#    GIT_TAG cee08ec38a8df7bc70397f10a4d30b9b33518bb4
+    GIT_REPOSITORY git://github.com/mariusmuja/flann
+    GIT_TAG 1.8.4
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
@@ -184,12 +234,16 @@ endmacro()
 # PCL fetch
 #
 macro(fetch_pcl)
-
   ExternalProject_Add(
     pcl-fetch
     SOURCE_DIR ${source_prefix}/pcl
-    GIT_REPOSITORY git://github.com/CambrianTech/pcl
-#    GIT_TAG origin/android-tag
+    # GIT_REPOSITORY git://github.com/patmarion/PCL.git
+    # GIT_REPOSITORY git://github.com/PointCloudLibrary/pcl.git
+    GIT_REPOSITORY git://github.com/Sirokujira/pcl.git
+    # GIT_TAG origin/android-tag
+    # GIT_TAG origin/master
+    # GIT_TAG pcl-1.8.0
+    GIT_TAG Branch_pcl-1.7.2
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
